62dca022b9efd2576ead6dc536bcc276f6351754
DerasChris <deras.christian011@gmail.com>
2024-05-19 14:19:38 -0600

    Añadiendo funcionalidad de davi negocios

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -1,39 +1,21 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Producto
- * 
- * @property int $id
- * @property int|null $id_emprendimiento
- * @property string|null $nombre_producto
- * @property int|null $cantidad_stock
- * @property float|null $precio
- *
- * @package App\Models
- */
 class Producto extends Model
 {
-	protected $table = 'productos';
-	public $timestamps = false;
+    use HasFactory;
 
-	protected $casts = [
-		'id_emprendimiento' => 'int',
-		'cantidad_stock' => 'int',
-		'precio' => 'float'
-	];
+    protected $table = 'productos';
 
-	protected $fillable = [
-		'id_emprendimiento',
-		'nombre_producto',
-		'cantidad_stock',
-		'precio'
-	];
+    protected $fillable = [
+        'nombre_producto',
+        'cantidad_stock',
+        'precio',
+    ];
+
+	public $timestamps = false; // Desactiva las marcas de tiempo
 }
